f24525c74b091359300d834f7fa1dfc4105f855b
rfkchrl <rfkchrl.03@gmail.com>
2020-09-24 15:21:11 +0700

    update

--- app/Http/Controllers/Admin/KuisionerController.php
+++ app/Http/Controllers/Admin/KuisionerController.php
@@ -4,60 +4,70 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-    //mengambil data kuisioner
+
+//mengambil data kuisioner
 use App\kuisionerdosen;
-    //meridericet keroute
+
+//meridericet keroute
 use Illuminate\Routing\Redirector;
 
 class KuisionerController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $pertanyaan = kuisionerdosen::all();
 
         // return view('adminlte/pertanyaan',['pertanyaan' =>$pertanyaan]);
-        return redirect()->route('pertanyaan.index');
+
+        return view('adminlte.pertanyaan', compact('pertanyaan'));
+        // return redirect()->route('pertanyaan.index');
     }
-    public function tambah(){
+    public function tambah()
+    {
         return view('adminlte/tambahpertanyaan');
     }
-    public function baru(Request $request){
-        $this->validate($request,[
-    		'id' => 'required',
+    public function baru(Request $request)
+    {
+        $this->validate($request, [
+            'id' => 'required',
             'pertanyaan' => 'required',
-            'aktif'=> 'required',
-            'choice'=>'required'
-    	]);
+            'aktif' => 'required',
+            'choice' => 'required'
+        ]);
 
         Pertanyaan::create([
-    		'id' => $request->id,
+            'id' => $request->id,
             'pertanyaan' => $request->pertanyaan,
             'aktif' => $request->aktif,
             'choice' => $request->choice
-    	]);
+        ]);
 
-    	return redirect('adminlte/pertanyaan');
+        return redirect('adminlte/pertanyaan');
     }
-    public function edit($id){
-        $pertanyaan = DB::table('kuisionerdosen')->where('id',$id)->get();
+    public function edit($id)
+    {
+        $pertanyaan = DB::table('kuisionerdosen')->where('id', $id)->get();
 
-        return view('adminlte/editpertanyaan',['pertanyaan' => $pertanyaan]);
+        return view('adminlte/editpertanyaan', ['pertanyaan' => $pertanyaan]);
     }
-    public function update($id,Request $request){
-        $this->validate($request,[
+    public function update($id, Request $request)
+    {
+        $this->validate($request, [
             'id' => 'required',
             'pertanyaan' => 'required',
-            'aktif'=> 'required',
+            'aktif' => 'required',
             'choice' => 'required'
-         ]);
+        ]);
 
-         $pertanyaan = Kuisionerdosen::find($id);
-         $pertanyaan->pertanyaan = $request->pertanyaan;
-         $pertanyaan->aktif = $request->aktif;
-         $pertanyaan->choice = $request->choice;
-         $pertanyaan->save();
-         return redirect('/pertanyaan');
+        $pertanyaan = Kuisionerdosen::find($id);
+        $pertanyaan->pertanyaan = $request->pertanyaan;
+        $pertanyaan->aktif = $request->aktif;
+        $pertanyaan->choice = $request->choice;
+        $pertanyaan->save();
+        return redirect('/pertanyaan');
     }
-    public function hapus($id){
+    public function hapus($id)
+    {
         $pertanyaan = kuisionerdosen::find($id);
         $pertanyaan->delete();
         return redirect('/pertanyaan');
