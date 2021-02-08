000eaf33da3aac8cc3d323b2dad929b47e24325a
@ridhobigboy29 <ridhobigboy@gmail.com>
2021-02-08 17:19:54 +0700

    revisi

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -14,20 +14,6 @@ use Carbon\Carbon;
 class ChartController extends Controller
 {
     public function index(){
-    $record = kuisionerkelas::select(DB::raw("COUNT(*) as count"), DB::raw("pertanyaan_id(created_at) as pertanyaan"),
-    DB::raw("jawaban_id(created_at) as jawaban"))
-    ->where('created_at','>','pertanyaan')
-    ->groupBy('pertanyaan_id','pertanyaan')
-    ->orderBy('pertanyaan');
-    // ->get();
-
-    $data = [];
-
-    foreach($record as $row) {
-        $data['label'][] = $row->pertanyaan_id;
-        $data['data'][] = (int) $row->count;
-     }
-     $data['chart_data'] = json_encode($data);
-     return view('adminlte.chart',$data);
+    return view('adminlte.chart');
     }
 }
--- resources/views/adminlte/chart.blade.php
+++ resources/views/adminlte/chart.blade.php
@@ -7,85 +7,22 @@
 @stop
 
 @section('conten')
-<div class="chart-container">
-    <div class="pie-chart-container">
-      <canvas id="pie-chart"></canvas>
-    </div>
-  </div>
-  <script>
-    $(function(){
-        //get the pie chart canvas
-        var cData = JSON.parse(`<?php echo $chart_data; ?>`);
-        var ctx = $("#pie-chart");
-
-        //pie chart data
-        var data = {
-          labels: cData.label,
-          datasets: [
-            {
-              label: "Users Count",
-              data: cData.data,
-              backgroundColor: [
-                "#DEB887",
-                "#A9A9A9",
-                "#DC143C",
-                "#F4A460",
-                "#2E8B57",
-                "#1D7A46",
-                "#CDA776",
-              ],
-              borderColor: [
-                "#CDA776",
-                "#989898",
-                "#CB252B",
-                "#E39371",
-                "#1D7A46",
-                "#F4A460",
-                "#CDA776",
-              ],
-              borderWidth: [1, 1, 1, 1, 1,1,1]
-            }
-          ]
-        };
-
-        //options
-        var options = {
-          responsive: true,
-          title: {
-            display: true,
-            position: "top",
-            text: "Last Week Registered Users -  Day Wise Count",
-            fontSize: 18,
-            fontColor: "#111"
-          },
-          legend: {
-            display: true,
-            position: "bottom",
-            labels: {
-              fontColor: "#333",
-              fontSize: 16
-            }
-          }
-        };
-
-        //create Pie Chart class object
-        var chart1 = new Chart(ctx, {
-          type: "pie",
-          data: data,
-          options: options
-        });
-
-    });
-  </script>
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>
 @stop
 
-@section('css')
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-<script>console.log('HI!')</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-@stop
