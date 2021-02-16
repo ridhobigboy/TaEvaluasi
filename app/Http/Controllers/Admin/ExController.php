00bdc53d946d69146e6f5e18652737882a763d9e
@ridhobigboy29 <ridhobigboy@gmail.com>
2021-02-16 22:55:52 +0700

    update grafik

--- app/Http/Controllers/Admin/ExController.php
+++ app/Http/Controllers/Admin/ExController.php
@@ -5,23 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\kuisionerkelas;
+use Illumninate\Support\Facades\DB;
 
 class ExController extends Controller
 {
     //
     public function index()
     {
-        $datas = kuisionerkelas::get();
+        // $datas = kuisionerkelas::get();
 
-        $setuju = [];
+        // $setuju = [];
 
-        foreach ($datas as $dp) {
-            $setuju[] = $dp->jawaban_id;
-        }
+        // foreach ($datas as $dp) {
+        //     $setuju[] = $dp->jawaban_id;
+        // }
         //  $baru = kuisionerkelas::select(\DB::raw("jawaban_id(*) as count"))
         //      ->whereJawaban('id', Jawaban("jawaban"))
         //      ->groupBy(\DB::raw("jawaban_id(created_at)"))
         //      ->pluck('count');
-        return view('adminlte.ex', compact('datas'));
+        return view('adminlte.ex');
     }
 }
--- resources/views/adminlte/ex.blade.php
+++ resources/views/adminlte/ex.blade.php
@@ -8,7 +8,9 @@
 
 @section('content')
 <h2 style="text-align: center;"><strong>Gong Xi Fachai</strong></h2>
-<div id="container"></div>
+<div id="container"></div><br><br><br><br>
+<div id="container2"></div><br><br><br><br>
+<div id="container3"></div>
 @stop
 
  @section('js')
@@ -47,15 +49,19 @@ Highcharts.chart('container', {
         name: 'Brands',
         colorByPoint: true,
         data: [{
-            name: 'Tidak Setuju',
-            y: {!! json_encode($datas) !!}
+            name: 'Sangat Tidak Setuju',
+            y: 10
         }, {
-            name: 'Agak Setuju',
+            name: 'Tidak Setuju',
             y: 10.85
         }, {
+            name: 'Agak Setuju',
+            y: 4.67
+        },{
             name: 'Setuju',
             y: 4.67
-        }, {
+        },
+        {
             name: 'Sangat Setuju',
             y: 4.18,
             sliced: true,
@@ -65,5 +71,128 @@ Highcharts.chart('container', {
 });
 </script>
 
+<script>
+    // Build the chart
+    Highcharts.chart('container2', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Stacked column chart'
+    },
+    xAxis: {
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total fruit consumption'
+        },
+        stackLabels: {
+            enabled: true,
+            style: {
+                fontWeight: 'bold',
+                color: ( // theme
+                    Highcharts.defaultOptions.title.style &&
+                    Highcharts.defaultOptions.title.style.color
+                ) || 'gray'
+            }
+        }
+    },
+    legend: {
+        align: 'right',
+        x: -30,
+        verticalAlign: 'top',
+        y: 25,
+        floating: true,
+        backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+    },
+    tooltip: {
+        headerFormat: '<b>{point.x}</b><br/>',
+        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    series: [{
+        name: 'John',
+        data: [5, 3, 4, 7, 2]
+    }, {
+        name: 'Jane',
+        data: [2, 2, 3, 2, 1]
+    }, {
+        name: 'Joe',
+        data: [3, 4, 4, 2, 5]
+    }]
+});
+</script>
+   <!-- word cloud chart -->
+ <script>
+Highcharts.chart('container3', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2017',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 60
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                distance: -50,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'white'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%'],
+            size: '110%'
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Sangat Tidak Setuju', 58.9],
+            ['Tidak Setuju', 13.29],
+            ['Agak Setuju', 13],
+            ['Setuju', 3.78],
+            ['Sangat Setuju', 3.42],
+            // {
+            //     name: 'Other',
+            //     y: 7.61,
+            //     dataLabels: {
+            //         enabled: false
+            //     }
+            // }
+        ]
+    }]
+});
+</script>
 @stop
 
