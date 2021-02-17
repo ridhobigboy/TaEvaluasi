4dc4f8d461bc965cf2f585778213f7f35aee0fdc
@ridhobigboy29 <ridhobigboy@gmail.com>
2021-02-17 18:23:35 +0700

    update terbaru

--- resources/views/adminlte/ex.blade.php
+++ resources/views/adminlte/ex.blade.php
@@ -7,7 +7,6 @@
 @stop
 
 @section('content')
-<h2 style="text-align: center;"><strong>Gong Xi Fachai</strong></h2>
 <div id="container"></div><br><br><br><br>
 <div id="container2"></div><br><br><br><br>
 <div id="container3"></div>
@@ -78,62 +77,303 @@ Highcharts.chart('container', {
         type: 'column'
     },
     title: {
-        text: 'Stacked column chart'
+        text: 'Perkuliahaan dimulai dan diakhiri tepat waktu '
+    },
+    subtitle: {
+        text: ''
+    },
+    accessibility: {
+        announceNewData: {
+            enabled: true
+        }
     },
     xAxis: {
-        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        type: 'category'
     },
     yAxis: {
-        min: 0,
         title: {
-            text: 'Total fruit consumption'
-        },
-        stackLabels: {
-            enabled: true,
-            style: {
-                fontWeight: 'bold',
-                color: ( // theme
-                    Highcharts.defaultOptions.title.style &&
-                    Highcharts.defaultOptions.title.style.color
-                ) || 'gray'
-            }
+            text: 'Modul Evaluasi Dosen'
         }
+
     },
     legend: {
-        align: 'right',
-        x: -30,
-        verticalAlign: 'top',
-        y: 25,
-        floating: true,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || 'white',
-        borderColor: '#CCC',
-        borderWidth: 1,
-        shadow: false
-    },
-    tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+        enabled: false
     },
     plotOptions: {
-        column: {
-            stacking: 'normal',
+        series: {
+            borderWidth: 0,
             dataLabels: {
-                enabled: true
+                enabled: true,
+                format: '{point.y:.1f}%'
             }
         }
     },
-    series: [{
-        name: 'John',
-        data: [5, 3, 4, 7, 2]
-    }, {
-        name: 'Jane',
-        data: [2, 2, 3, 2, 1]
-    }, {
-        name: 'Joe',
-        data: [3, 4, 4, 2, 5]
-    }]
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+    },
+
+    series: [
+        {
+            name: "Browsers",
+            colorByPoint: true,
+            data: [
+                {
+                    name: "Sangat Tidak Setuju",
+                    y: 62.74,
+                    drilldown: "Sangat Tidak Setuju"
+                },
+                {
+                    name: "Tidak Setuju",
+                    y: 10.57,
+                    drilldown: "Tidak Setuju"
+                },
+                // {
+                //     name: "Internet Explorer",
+                //     y: 7.23,
+                //     drilldown: "Internet Explorer"
+                // },
+                {
+                    name: "Agak Setuju",
+                    y: 5.58,
+                    drilldown: "Agak Setuju"
+                },
+                {
+                    name: "Setuju",
+                    y: 4.02,
+                    drilldown: "Setuju"
+                },
+                {
+                    name: "Sangat Setuju",
+                    y: 1.92,
+                    drilldown: "Sangat Setuju"
+                }
+            ]
+        }
+    ],
+    drilldown: {
+        series: [
+            {
+                name: "Chrome",
+                id: "Chrome",
+                data: [
+                    [
+                        "v65.0",
+                        0.1
+                    ],
+                    [
+                        "v64.0",
+                        1.3
+                    ],
+                    [
+                        "v63.0",
+                        53.02
+                    ],
+                    [
+                        "v62.0",
+                        1.4
+                    ],
+                    [
+                        "v61.0",
+                        0.88
+                    ],
+                    [
+                        "v60.0",
+                        0.56
+                    ],
+                    [
+                        "v59.0",
+                        0.45
+                    ],
+                    [
+                        "v58.0",
+                        0.49
+                    ],
+                    [
+                        "v57.0",
+                        0.32
+                    ],
+                    [
+                        "v56.0",
+                        0.29
+                    ],
+                    [
+                        "v55.0",
+                        0.79
+                    ],
+                    [
+                        "v54.0",
+                        0.18
+                    ],
+                    [
+                        "v51.0",
+                        0.13
+                    ],
+                    [
+                        "v49.0",
+                        2.16
+                    ],
+                    [
+                        "v48.0",
+                        0.13
+                    ],
+                    [
+                        "v47.0",
+                        0.11
+                    ],
+                    [
+                        "v43.0",
+                        0.17
+                    ],
+                    [
+                        "v29.0",
+                        0.26
+                    ]
+                ]
+            },
+            {
+                name: "Firefox",
+                id: "Firefox",
+                data: [
+                    [
+                        "v58.0",
+                        1.02
+                    ],
+                    [
+                        "v57.0",
+                        7.36
+                    ],
+                    [
+                        "v56.0",
+                        0.35
+                    ],
+                    [
+                        "v55.0",
+                        0.11
+                    ],
+                    [
+                        "v54.0",
+                        0.1
+                    ],
+                    [
+                        "v52.0",
+                        0.95
+                    ],
+                    [
+                        "v51.0",
+                        0.15
+                    ],
+                    [
+                        "v50.0",
+                        0.1
+                    ],
+                    [
+                        "v48.0",
+                        0.31
+                    ],
+                    [
+                        "v47.0",
+                        0.12
+                    ]
+                ]
+            },
+            // {
+            //     name: "Internet Explorer",
+            //     id: "Internet Explorer",
+            //     data: [
+            //         [
+            //             "v11.0",
+            //             6.2
+            //         ],
+            //         [
+            //             "v10.0",
+            //             0.29
+            //         ],
+            //         [
+            //             "v9.0",
+            //             0.27
+            //         ],
+            //         [
+            //             "v8.0",
+            //             0.47
+            //         ]
+            //     ]
+            // },
+            {
+                name: "Safari",
+                id: "Safari",
+                data: [
+                    [
+                        "v11.0",
+                        3.39
+                    ],
+                    [
+                        "v10.1",
+                        0.96
+                    ],
+                    [
+                        "v10.0",
+                        0.36
+                    ],
+                    [
+                        "v9.1",
+                        0.54
+                    ],
+                    [
+                        "v9.0",
+                        0.13
+                    ],
+                    [
+                        "v5.1",
+                        0.2
+                    ]
+                ]
+            },
+            {
+                name: "Edge",
+                id: "Edge",
+                data: [
+                    [
+                        "v16",
+                        2.6
+                    ],
+                    [
+                        "v15",
+                        0.92
+                    ],
+                    [
+                        "v14",
+                        0.4
+                    ],
+                    [
+                        "v13",
+                        0.1
+                    ]
+                ]
+            },
+            {
+                name: "Opera",
+                id: "Opera",
+                data: [
+                    [
+                        "v50.0",
+                        0.96
+                    ],
+                    [
+                        "v49.0",
+                        0.82
+                    ],
+                    [
+                        "v12.1",
+                        0.14
+                    ]
+                ]
+            }
+        ]
+    }
 });
+
 </script>
    <!-- word cloud chart -->
  <script>
@@ -144,10 +384,10 @@ Highcharts.chart('container3', {
         plotShadow: false
     },
     title: {
-        text: 'Browser<br>shares<br>2017',
+        text: 'Diskusi materi dan pembahasan tugas/ujian berjalan baik, didalam/luar kelas',
         align: 'center',
         verticalAlign: 'middle',
-        y: 60
+        y: 115
     },
     tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
